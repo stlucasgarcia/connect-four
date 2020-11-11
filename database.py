e57f4823a3cb24a83e31676a23e36dc808d15bd5
Chang <61498020+ChangProg@users.noreply.github.com>
2020-11-11 11:18:25 -0300

    Updated database.py

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ import sqlite3
 
 class ScoreboardData:
     def __init__(self, data:dict):
-        self.score_1:int, self.score_2:int = data.values()
-        self.name_1:str, self.name_2:str = data.keys()
+        self.score_1, self.score_2 = data.values()
+        self.name_1 , self.name_2 = data.keys()
         self.winner:tuple = (self.name_1, self.score_1) if self.score_1 >= self.score_2 else (self.name_2, self.score_2)
     
     def connect(self):
@@ -37,87 +37,43 @@ class ScoreboardData:
         conn.commit()
         conn.close()
         print("Done.")
+    
+    def getData(self, amount=0):
+        conn = sqlite3.connect("scores.db")
+        c = conn.cursor()
+        query = "SELECT * FROM scores ORDER BY wins DESC, name ASC;"
+        c.execute(query)
+        rows = c.fetchall()
+
+        leader_board_full = []
+        for row in rows:
+            leader_board_full.append((row[1],row[2]))
+
+        leader_board = []
+        for i in range(amount):
+            leader_board.append(leader_board_full[i])
+        
+        return leader_board
 
 
+# data = {
+#     "Nick":0,
+#     "John":1,
+# }
+# data = {
+#     "George":1,
+#     "Michael":0,
+# }
 data = {
-    "Roberto":0,
-    "Arnaldo":1,
+    "Fred":1,
+    "Peter":0,
 }
 a = ScoreboardData(data)
 
-#a.connect()
+a.connect()
 #a.updateTable()
 
 a.winnerUpdate()
-    
 
-
-
-
-
-
-
-
-
-# import pandas as pd
-
-# class ScoreCSV:
-#     def __init__(self, data):
-#         self.score_1 = data[0]
-#         self.name_1 = data[1]
-#         self.score_2 = data[2]
-#         self.name_2 = data[3]
-    
-#     def exportCSV(self):
-        
-#         pd.DataFrame([[self.name_1,self.score_1],[self.name_2,self.score_2]],columns=["Player", "Wins"])
-
-
-
-# data = [5,"Pedro",10,"Rodrigo"]
-
-# obj = ScoreCSV(data)
-
-
-# obj.exportCSV()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import psycopg2
-
-# #establishing the connection
-# conn = psycopg2.connect(
-#    database="postgres", user='postgres', password='djbc!2001HQ', host='127.0.0.1', port= '5432'
-# )
-# conn.autocommit = True
-
-# #Creating a cursor object using the cursor() method
-# cursor = conn.cursor()
-
-# #Preparing query to create a database
-# sql = '''CREATE database mydb;'''
-
-# #Creating a database
-# cursor.execute(sql)
-# print("Database created successfully........")
-
-# #Closing the connection
-# conn.close()
+x = a.getData(3)
+print(x)
